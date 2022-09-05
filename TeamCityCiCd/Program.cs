299cfe095e9e2eb03d852bfb29152320d62aaf01
TheIncredibleIllmo <eduardomedm@gmail.com>
2022-09-05 17:33:14 -0500

    Swagger description text

--- TeamCityCiCd/Program.cs
+++ TeamCityCiCd/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c=>c.SwaggerDoc("v1", new OpenApiInfo { Title="WeatherForecast.API", Version="v1", Description="Test"}));
+builder.Services.AddSwaggerGen(c=>c.SwaggerDoc("v1", new OpenApiInfo { Title="WeatherForecast.API", Version="v1", Description="Team City CI/CD"}));
 
 var app = builder.Build();
 
